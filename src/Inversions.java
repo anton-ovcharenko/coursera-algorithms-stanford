239afb2cb13885404f24b983525c5dc6e187c323
Anton Ovcharenko <888_666@rambler.ru>
2017-10-01 18:31:15 +0300

    fix inversion for big arrays

--- src/Inversions.java
+++ src/Inversions.java
@@ -1,10 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Inversions {
 
     public static void main(String[] args) {
-        System.out.println(inversions(new int[]{1,3,5,2,4,6}).inversionCount);
-        System.out.println(inversions(new int[]{6,5,4,3,2,1}).inversionCount);
+        //System.out.println(inversions(new int[]{1,3,5,2,4,6}).inversionCount);
+        //System.out.println(inversions(new int[]{6,5,4,3,2,1}).inversionCount);
+
+        try (Scanner scanner = new Scanner(new File("./res/IntegerArray.txt"))) {
+            int[] array = new int[100000];
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                array[i++] = scanner.nextInt();
+            }
+
+            System.out.println(inversions(array).inversionCount);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private static InversionResult inversions(int[] array) {
@@ -23,7 +38,8 @@ public class Inversions {
 
     private static InversionResult merge(int[] left, int[] right) {
         int[] result = new int[left.length + right.length];
-        int leftIndex = 0, rightIndex = 0, inversionCount = 0;
+        int leftIndex = 0, rightIndex = 0;
+        long inversionCount = 0;
 
         for (int k = 0; k < result.length; k++) {
             if (rightIndex >= right.length ||
@@ -39,10 +55,10 @@ public class Inversions {
     }
 
     static class InversionResult {
-        int inversionCount;
+        long inversionCount;
         int[] sortedArray;
 
-        InversionResult(int inversionCount, int[] soretdArray) {
+        InversionResult(long inversionCount, int[] soretdArray) {
             this.inversionCount = inversionCount;
             this.sortedArray = soretdArray;
         }
